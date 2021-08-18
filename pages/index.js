6d94468a3b8a0aa8edf729415d8be6b008bfaaeb
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-18 13:54:13 +0530

    adding server side props

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,107 @@
-import Head from 'next/head'
+import { getSession } from "next-auth/client";
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import MovieFranchise from "../components/MovieFranchise";
+import ShowCollection from "../components/Movies/ShowColllection";
+import MovieCollection from "../components/Movies/MovieCollection";
+// import Movies from "../components/Movies/MovieCollection";
+import Slider from "../components/Slider";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default function Home({
+  session,
+  popularMovies,
+  popularShows,
+  topRatedMovies,
+  topRatedShows,
+}) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Disney+ || Jestor Nogueiro</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      {/* {session ? <Movies /> : <Hero />} */}
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      <main className="relative min-h-screen after:bg-homeImage after:bg-cover after:bg-no-repeat after:bg-fixed after:absolute after:inset-0 after:z-[-1]">
+        <Slider />
+        <MovieFranchise />
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        {/* popular and top rated movies props */}
+        <MovieCollection
+          title={"Popular Movies"}
+          ResponseData={popularMovies}
+        />
+        <MovieCollection
+          title={"Top Rated Movies"}
+          ResponseData={topRatedMovies}
+        />
+        {/* popular movies props  */}
+        <ShowCollection title={"Popular Shows"} ResponseData={popularShows} />
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ShowCollection
+          title={"Top Rated Shows"}
+          ResponseData={topRatedShows}
+        />
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  // const session = await getSession(context);
+
+  // Multiple fech request
+  const [
+    popularMovies,
+    popularShows,
+    topRatedMovies,
+    topRatedShows,
+  ] = await Promise.all([
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json()),
+    fetch(
+      `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json()),
+    fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json()),
+    fetch(
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json()),
+  ]);
+  //response we get
+  // const [
+  // popularMovies,
+  // popularShows,
+  // topRatedMovies,
+  // topRatedShows,
+  // ] = await Promise.all([
+  // converting the response in json
+  // popularMovies.json(),
+  //   popularShows.json(),
+  //   topRatedMovies.json(),
+  //   topRatedShows.json(),
+  // ]);
+  // OR you can do it in traditional way of fetching the data like so:
+  // const request = await fetch(
+  //   `https://api.themoviedb.org/3${
+  //     requests[genre]?.url || requests.fetchTrending.url
+  //   }`
+  // ).then((res) => res.json());
+
+  return {
+    props: {
+      // session,
+      popularMovies: popularMovies.results,
+      popularShows: popularShows.results,
+      topRatedMovies: topRatedMovies.results,
+      topRatedShows: topRatedShows.results,
+    },
+  };
 }
