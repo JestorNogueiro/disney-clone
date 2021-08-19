0a2a6246f096cebf0fbb7ead8c0fe3249c65929e
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-19 23:38:35 +0530

    Added footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,29 +1,42 @@
 import Link from "next/link";
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+
 function Footer() {
   return (
-    <section className="h-16 border-white/60 px-8 py-2.5 border-t-2 rounded">
-      <h3 className="">Design & Developed by @jestor nogueiro</h3>
-      <div className="flex flex-row space-x-2.5">
-        <ul className="list-none mt-2">
-          Social Media
-          <li className="px-4">
+    <section className="pt-2 text-center border-white/60  border-t-2 rounded mb-4">
+      <span className="text-white/60 ">Developed by </span>
+      <span className="font-bold tracking-widest border-b ">
+        @ jestor nogueiro
+      </span>
+
+      <div className=" mt-3">
+        <ul className="list-none  flex items-center justify-center">
+          <li className="px-4 hover:scale-100">
             <Link href="https://www.facebook.com/jestor.nogueiro/">
-              <a target="_blank">facebook</a>
+              <a target="_blank">
+                <FaFacebook className="text-2xl hover:scale-100" />
+              </a>
             </Link>
           </li>
           <li className="px-4">
             <Link href="https://www.instagram.com/jestor_nogueiro/">
-              <a target="_blank">Insta</a>
+              <a target="_blank">
+                <FaInstagram className="text-2xl" />
+              </a>
             </Link>
           </li>
           <li className="px-4">
             <Link href="https://www.linkedin.com/in/jestor-nogueiro-02467b184/">
-              <a target="_blank">linkedIn</a>
+              <a target="_blank">
+                <FaLinkedin className="text-2xl" />
+              </a>
             </Link>
           </li>
           <li className="px-4">
             <Link href="https://github.com/JestorNogueiro">
-              <a target="_blank">Github</a>
+              <a target="_blank">
+                <FaGithub className="text-2xl" />
+              </a>
             </Link>
           </li>
         </ul>
