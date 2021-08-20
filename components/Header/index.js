c6f5f22e66e47f8607f8c0c6a882c13085deee35
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-20 18:02:16 +0530

    edited header items

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { signIn, signOut, useSession } from "next-auth/client";
+// import { signIn, signOut, useSession } from "next-auth/client";
 import {
   HomeIcon,
   PlusIcon,
@@ -9,10 +9,10 @@ import {
 import { useRouter } from "next/router";
 
 const Header = () => {
-  const [session, loading] = useSession();
+  // const [session, loading] = useSession();
   const router = useRouter();
   return (
-    <header className="flex items-center sticky top-0 z-[1000] h-20 px-10 bg-[#040714] md:px-12">
+    <header className="flex items-center sticky top-0 z-[1000] h-16 px-8 bg-[#040714] md:px-12">
       <Image
         src="/images/logo.svg"
         height="80px"
@@ -21,21 +21,21 @@ const Header = () => {
         onClick={() => router.push("/")}
       />
       {/* {session && ( */}
-      <div className=" hidden ml-10 md:flex">
+      <div className=" hidden ml-10 md:flex md:space-2 md:gap-3.5">
         <a className="text-white header-link group">
           <HomeIcon className="h-5" />
-          <span className="span">Home</span>
+          <span className="span mx-4">Home</span>
         </a>
         <a className="text-white header-link group">
           <SearchIcon className="h-5" />
-          <span className="span">search</span>
+          <span className="span mx-4">search</span>
         </a>
         <a className="text-white header-link group">
           <PlusIcon className="h-5" />
           <span className="span">watchlist</span>
         </a>
         <a className="text-white header-link group">
-          <StarIcon className="h-5" />
+          <StarIcon className="h-5 my-2" />
           <span className="span">originals</span>
         </a>
         <a className="text-white header-link group">
@@ -58,17 +58,17 @@ const Header = () => {
         LogIn
       </button>
       {/* ) : ( */}
-      <>
-        <img
-          // src={session.user?.image}
-          src="/images/logo.svg"
-          className="rounded-full object-cover cursor-pointer ml-auto h-12 "
-        />
+      <div>
+        {/* <img
+            src={session.user?.image}
+            src="/images/logo.svg"
+            className="rounded-full object-cover cursor-pointer ml-auto h-12 "
+          /> */}
         {/* <p className="px-5">Hello,{session.user?.name}</p> */}
-        {/* <h3 onClick={signOut} className="cursor-pointer">
-          Sign Out
-        </h3> */}
-      </>
+        {/* <h3 onClick={signOut} className="cursor-pointer"> */}
+        {/* Sign Out */}
+        {/* </h3> */}
+      </div>
       {/* )} */}
     </header>
   );
