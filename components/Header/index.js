171b4cdfc3a82fa84e1ba75be42c290cd256bb4b
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-20 23:27:20 +0530

    removed unwanted code

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-// import { signIn, signOut, useSession } from "next-auth/client";
 import {
   HomeIcon,
   PlusIcon,
@@ -9,7 +8,6 @@ import {
 import { useRouter } from "next/router";
 
 const Header = () => {
-  // const [session, loading] = useSession();
   const router = useRouter();
   return (
     <header className="flex items-center sticky top-0 z-[1000] h-16 px-8 bg-[#040714] md:px-12">
@@ -21,7 +19,7 @@ const Header = () => {
         className="cursor-pointer"
         onClick={() => router.push("/")}
       />
-      {/* {session && ( */}
+
       <div className=" hidden ml-10 md:flex md:space-2 md:gap-3.5">
         <a className="text-white header-link group">
           <HomeIcon className="h-5" />
@@ -44,33 +42,15 @@ const Header = () => {
           <span className="span">movies</span>
         </a>
         <a className="text-white header-link group">
-          <img src="/images/series-icon.svg" className="h-5" />
+          <img src="/images/series-icon.svg" className="h-5" alt="icon" />
 
           <span className="span">series</span>
         </a>
       </div>
-      {/* )} */}
 
-      {/* {!session ? ( */}
-      <button
-        className="ml-auto uppercase border px-3.5 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
-        // onClick={signIn}
-      >
+      <button className="ml-auto uppercase border px-3.5 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200">
         LogIn
       </button>
-      {/* ) : ( */}
-      <div>
-        {/* <img
-            src={session.user?.image}
-            src="/images/logo.svg"
-            className="rounded-full object-cover cursor-pointer ml-auto h-12 "
-          /> */}
-        {/* <p className="px-5">Hello,{session.user?.name}</p> */}
-        {/* <h3 onClick={signOut} className="cursor-pointer"> */}
-        {/* Sign Out */}
-        {/* </h3> */}
-      </div>
-      {/* )} */}
     </header>
   );
 };
