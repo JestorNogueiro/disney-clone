f456973e4513144d87722aab4bfcac2c44c43f3c
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-20 16:42:55 +0530

    customized for preloader

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,46 @@
 import "tailwindcss/tailwind.css";
 import "../styles.css";
 import { Provider } from "next-auth/client";
+import Router from "next/router";
+import Head from "next/head";
+import Nprogress from "nprogress";
+import Loader from "../components/Loader";
+import { useState } from "react/cjs/react.development";
 
 function MyApp({ Component, pageProps }) {
+  // const Router = useRouter();
+  Nprogress.configure({ showSpinner: true });
+
+  const [loader, SetLoader] = useState(false);
+
+  Router.events.on("routeChangeStart", (url) => {
+    console.log("route is changing");
+    SetLoader(true);
+    Nprogress.start();
+  });
+
+  Router.events.on("routeChangeComplete", (url) => {
+    console.log("route is complete");
+    SetLoader(false);
+    Nprogress.done();
+  });
+
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+        {loader && <Loader />}
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
 
