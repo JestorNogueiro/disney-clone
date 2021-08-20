a438075c1776cabc86c8a2bbffdb4dc444582702
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-20 23:40:12 +0530

    minnor error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }) {
           href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
           integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
           crossOrigin="anonymous"
-          referrerpolicy="no-referrer"
+          referrerPolicy="no-referrer"
         />
       </Head>
       {loader && <Loader />}
