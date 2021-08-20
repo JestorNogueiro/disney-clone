b60a38d4f9c3120038da1d391933208c10c145c9
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-20 18:04:27 +0530

    hero items

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 function Hero() {
   return (
-    <section>
+    <section className="relative">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <title>Log in || Disney+</title>
@@ -23,7 +23,20 @@ function Hero() {
             height="150"
             objectFit="contain"
           />
-          <button>Get All There</button>
+          <button className="bg-blue-600 uppercase text-xl tracking-wide font-extrabold py-4 px-6 w-full rounded hover:bg-[#0485ee]">
+            Get all there
+          </button>
+          <p className="text-xs text-center ">
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $
+          </p>
+          <Image
+            src="/images/cta-logo-two.png"
+            width="600"
+            height="70"
+            objectFit="contain"
+          />
         </div>
       </div>
     </section>
